163676104f0b073580f310ba29b826c19dac7d33
altr0616 <aleksandr.trushchinskii@netcracker.com>
2018-07-11 08:54:06 +0300

    Refactoring

--- app/src/main/java/ru/aleksandrtrushchinskii/surfproject/ui/fragment/CreateEditFragment.kt
+++ app/src/main/java/ru/aleksandrtrushchinskii/surfproject/ui/fragment/CreateEditFragment.kt
@@ -9,13 +9,18 @@ import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.create_edit_fragment.*
 import ru.aleksandrtrushchinskii.surfproject.R
 import ru.aleksandrtrushchinskii.surfproject.common.tools.ACTION_KEY
-import ru.aleksandrtrushchinskii.surfproject.common.tools.configurateDate
 import ru.aleksandrtrushchinskii.surfproject.common.tools.inflateBinding
+import ru.aleksandrtrushchinskii.surfproject.common.tools.invisible
+import ru.aleksandrtrushchinskii.surfproject.common.tools.visible
 import ru.aleksandrtrushchinskii.surfproject.databinding.CreateEditFragmentBinding
+import ru.aleksandrtrushchinskii.surfproject.model.entity.Todo
 import ru.aleksandrtrushchinskii.surfproject.ui.component.LoadingState
 import ru.aleksandrtrushchinskii.surfproject.ui.component.ViewModelFactory
-import javax.inject.Inject
+import ru.aleksandrtrushchinskii.surfproject.ui.component.picker.DatePickerFragment
+import ru.aleksandrtrushchinskii.surfproject.ui.component.picker.TimePickerFragment
 import ru.aleksandrtrushchinskii.surfproject.ui.viewmodel.TodoViewModel
+import java.util.*
+import javax.inject.Inject
 
 
 class CreateEditFragment : DaggerFragment() {
@@ -25,6 +30,9 @@ class CreateEditFragment : DaggerFragment() {
 
     private lateinit var binding: CreateEditFragmentBinding
     private lateinit var viewModel: TodoViewModel
+
+    private var datePickerFragment: DatePickerFragment? = null
+    private var timePickerFragment: TimePickerFragment? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -41,15 +49,96 @@ class CreateEditFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        configurateDate(fragmentManager!!, viewModel,
-                todoNotificationDateTextView,
-                todoNotificationTimeTextView,
-                todoNotificationDateButton,
-                todoNotificationTimeButton,
-                todoNotificationClearButton
-        )
+        if (arguments!!.getString(ACTION_KEY)!! == "create") {
+            viewModel.todo.value = Todo()
+        }
+
+        configurateDate()
 
         LoadingState.stop()
+    }
+
+    private fun configurateDate() {
+        if (viewModel.todo.value?.notification != null) {
+            val c = Calendar.getInstance()
+
+            c.time = viewModel.todo.value?.notification
+
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val hour = c.get(Calendar.HOUR_OF_DAY)
+            val minute = c.get(Calendar.MINUTE)
+
+            if (year != null && month != null && month != day) {
+                todoNotificationDateTextView.text = "$day:$month:$year"
+            }
+
+            if (hour != null && minute != null) {
+                todoNotificationTimeTextView.text = "$hour:$minute"
+            }
+        } else {
+            todoNotificationClearButton?.invisible()
+        }
+
+        todoNotificationDateButton?.setOnClickListener {
+            datePickerFragment = DatePickerFragment()
+            datePickerFragment?.listener = { year, month, dayOfMonth ->
+                val calendar = Calendar.getInstance()
+
+                if (viewModel.todo.value?.notification != null) {
+                    calendar.time = viewModel.todo.value?.notification
+                }
+
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                viewModel.todo.value!!.notification = calendar.time
+
+                todoNotificationDateTextView.text = "$dayOfMonth:$month:$year"
+
+                todoNotificationClearButton?.visible()
+            }
+            datePickerFragment?.show(fragmentManager, "datePicker")
+        }
+
+        todoNotificationTimeButton?.setOnClickListener {
+            timePickerFragment = TimePickerFragment()
+
+            timePickerFragment?.listener = { hourOfDay, minute ->
+                val calendar = Calendar.getInstance()
+
+                if (viewModel.todo.value?.notification != null) {
+                    calendar.time = viewModel.todo.value?.notification
+                }
+
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                calendar.set(Calendar.MINUTE, minute)
+
+                viewModel.todo.value!!.notification = calendar.time
+
+                todoNotificationTimeTextView.text = "$hourOfDay:$minute"
+
+                todoNotificationClearButton?.visible()
+            }
+
+            timePickerFragment?.show(fragmentManager, "timePicker")
+        }
+
+        todoNotificationClearButton?.setOnClickListener {
+            viewModel.todo.value?.notification = null
+            todoNotificationDateTextView.text = ""
+            todoNotificationTimeTextView.text = ""
+            it.invisible()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        datePickerFragment?.dismiss()
+        timePickerFragment?.dismiss()
     }
 
 }
